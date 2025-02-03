58991b2afb8d8f13d1fe8b628d1fff1e074f20e6
Roberto <rob.cerminara@gmail.com>
2025-02-03 19:36:29 +0100

    first commit

---
+++ Temp.R
@@ -0,0 +1,17 @@
+library(readr)####
+library(sf)
+library(ggplot2)
+library(dplyr)
+library(maps)
+library(tidyr)
+library(RColorBrewer)
+library(stringr)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(ggrepel)
+library(lubridate)
+
+
+################################################
+Ds=read.csv("Earthquake_1970-2025.csv")
+View(Ds)
